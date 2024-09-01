8bf5547aaea41ba185fcff56072a751a66c78b86
xennon-sudo <rdibyendu091@gmail.com>
2024-09-01 22:36:49 +0600

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,4 +2,5 @@
 set -ex
 
 # Start gunicorn, listening on port 500, access log to stdout
-exec gunicorn -w 4 -b '0.0.0.0:5000' --access-logfile=- 'app:app'
+#exec gunicorn -w 4 -b '0.0.0.0:5000' --access-logfile=- 'app:app'
+exec code-server --host 0.0.0.0 --port 5000
